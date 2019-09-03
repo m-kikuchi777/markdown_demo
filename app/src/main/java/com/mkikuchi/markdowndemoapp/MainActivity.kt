5f8fee6be9c22bb9c4788e7f0017ee21a4b86c01
m-kikuchi777 <tnct.km.777@gmail.com>
2019-09-03 23:23:11 +0900

    Markdownのプレビューを表示する処理を追加

--- app/src/main/java/com/mkikuchi/markdowndemoapp/MainActivity.kt
+++ app/src/main/java/com/mkikuchi/markdowndemoapp/MainActivity.kt
@@ -2,11 +2,38 @@ package com.mkikuchi.markdowndemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.noties.markwon.Markwon
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val markwon = Markwon.create(this)
+
+        try {
+            val cheatsheet = assets.open("Markdown-Here-Cheatsheet.md")
+            val br = BufferedReader(InputStreamReader(cheatsheet))
+
+            var text = ""
+
+            while (true) {
+                val str = br.readLine()
+                if (str != null) {
+                    text += str + "\n"
+                } else {
+                    break
+                }
+            }
+
+            markwon.setMarkdown(markdownTextView, text)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
